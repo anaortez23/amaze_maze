faf4d0721873e1c9c82ddf83ed643ad85b5cd8e2
Ana Ortez <anaortez23@gmail.com>
2021-12-16 10:52:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a>
+          Deployment is happening!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
